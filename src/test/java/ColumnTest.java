42ccf9cc574eac5e76ef222680a081422c34e95f
combegae <gc26@outlook.fr>
2024-04-17 08:27:02 +0200

    Rajout de tests Column et Dataframe

--- src/test/java/ColumnTest.java
+++ src/test/java/ColumnTest.java
@@ -7,23 +7,26 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ColumnTest {
 
-    @Test
-    public void constructorInt() {
+    @Test /* test le constructeur avec un type int */
+    public void constructorInt() { 
         Types typeInt = Types.INT;
         Column<Integer> Column = new Column<Integer>(typeInt);
         Types colType = Column.getType();
         assertTrue(Types.INT == colType);
     }
 
+    @Test /* test le constructeur avec un type float */
     public void constructorFloat() {
         Types typeFloat = Types.FLOAT;
         Column<Integer> Column = new Column<Integer>(typeFloat);
         Types colType = Column.getType();
         assertTrue(Types.FLOAT == colType);
     }
+
+    @Test /* test le constructeur avec un type string */
     public void constructorTest() {
-        Types typeInt = Types.FLOAT;
-        Column<Integer> Column = new Column<Integer>(typeInt);
+        Types typeInt = Types.STRING;
+        Column<String> Column = new Column<String>(typeInt);
         Types colType = Column.getType();
         assertTrue(Types.STRING == colType);
     }
--- src/test/java/DataframeTest.java
+++ src/test/java/DataframeTest.java
@@ -1,45 +1,260 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
 import java.util.*;
 
 public class DataframeTest {
 
-    private class TestingData{
-        int x;
-        int y;
-
-        public TestingData(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-
 
     private List<String> name = new ArrayList<>();
     private List<Column> datas = new ArrayList<>();
+
     @BeforeEach
-    public void init(){
-        for(int i =0; i < 3; i++)
-            name.add("Colonne" + i);
+    public void init() {
 
-        List<Integer> numbers = new ArrayList<>();
-        for(int i = 0 ; i < 5; i++){
-            numbers.add(i);
+        Types typeInt = Types.INT;
+        Column<Integer> ColumnID = new Column<Integer>(typeInt);
+
+        Types typeStringNom = Types.STRING;
+        Column<String> ColumnNom = new Column<String>(typeStringNom);
+
+        Types typeStringPrenom = Types.STRING;
+        Column<String> ColumnPrenom = new Column<String>(typeStringPrenom);
+
+        Types typeFloat = Types.FLOAT;
+        Column<Float> ColumnNote = new Column<Float>(typeFloat);
+
+        for (int i = 0; i < 4; i++) {
+            ColumnID.add(i + 1);
         }
-        List<String> names = new ArrayList<>();
-        names.add("Fabien");
-        names.add("Alban");
-        names.add("Gaétan");
-        names.add("Raphaël");
 
-        List<Object> objets = new ArrayList<>();
-        objets.add(new TestingData(1, 3));
-        objets.add(new TestingData(3, 4));
+        ColumnPrenom.add("Alban");
+        ColumnPrenom.add("Gaetan");
+        ColumnPrenom.add("Fabien");
+        ColumnPrenom.add("Raphael");
 
+        ColumnNom.add("Faurie");
+        ColumnNom.add("Combe");
+        ColumnNom.add("Vial");
+        ColumnNom.add("Balssa");
+
+        ColumnNote.add((float)45.2);
+        ColumnNote.add((float)48.0);
+        ColumnNote.add((float)0.1);
+        ColumnNote.add((float) 1257854.987457);
+        
+        datas.add(ColumnID);
+        datas.add(ColumnPrenom);
+        datas.add(ColumnNom);
+        datas.add(ColumnNote);
+        
+        
+        List<String> names = new ArrayList<String>();
+        names.add("ID");
+        names.add("Prenom");
+        names.add("Nom");
+        names.add("Note");
     }
+    
+
+    @Test /* Test qui prend 4 colonnes non vides de meme taille et des 3 types possibles  */
+    void constructorTestParCol3Types() {
+        assertDoesNotThrow(() -> {
+            DataFrame df = new DataFrame(name, datas);
+        });
+    }
+
+
+    @Test /* Test qui prend 3 colonnes non vides  */
+    void constructorTestColElementNull() {
+        assertDoesNotThrow(() -> {
+            Types typeInt = Types.INT;
+            Column<Integer> ColumnID = new Column<Integer>(typeInt);
+
+            Types typeStringNom = Types.STRING;
+            Column<String> ColumnNom = new Column<String>(typeStringNom);
+
+            Types typeStringPrenom = Types.STRING;
+            Column<String> ColumnPrenom = new Column<String>(typeStringPrenom);
+
+            Types typeFloat = Types.FLOAT;
+            Column<Float> ColumnNote = new Column<Float>(typeFloat);
+
+            for (int i = 0; i < 4; i++) {
+                ColumnID.add(i + 1);
+            }
+
+            ColumnPrenom.add("Alban");
+            ColumnPrenom.add("Gaetan");
+            ColumnPrenom.add("Fabien");
+            ColumnPrenom.add("Raphael");
+
+            ColumnNom.add("Faurie");
+            ColumnNom.add("Combe");
+            ColumnNom.add(null);
+            ColumnNom.add("Balssa");
+
+            ColumnNote.add((float) 45.2);
+            ColumnNote.add((float) 48.0);
+            ColumnNote.add((float) 0.1);
+            ColumnNote.add((float) 1257854.987457);
+
+            datas.add(ColumnID);
+            datas.add(ColumnPrenom);
+            datas.add(ColumnNom);
+            datas.add(ColumnNote);
+
+            List<String> names = new ArrayList<String>();
+            names.add("ID");
+            names.add("Prenom");
+            names.add("Nom");
+            names.add("Note");
+
+            DataFrame df = new DataFrame(name, datas);
+        });
+    }
+
     @Test
-    void constructorTest(){
-        DataFrame df = new DataFrame(name, datas);
+    public void testColVide() {
+        assertDoesNotThrow(() -> {
+            Types typeInt = Types.INT;
+            Column<Integer> ColumnID = new Column<Integer>(typeInt);
+
+            Types typeStringNom = Types.STRING;
+            Column<String> ColumnNom = new Column<String>(typeStringNom);
+
+            Types typeStringPrenom = Types.STRING;
+            Column<String> ColumnPrenom = new Column<String>(typeStringPrenom);
+
+            Types typeFloat = Types.FLOAT;
+            Column<Float> ColumnNote = new Column<Float>(typeFloat);
+
+            for (int i = 0; i < 4; i++) {
+                ColumnID.add(i + 1);
+            }
+
+            ColumnNom.add("Faurie");
+            ColumnNom.add("Combe");
+            ColumnNom.add(null);
+            ColumnNom.add("Balssa");
+
+            ColumnNote.add((float) 45.2);
+            ColumnNote.add((float) 1257854.987457);
+
+            datas.add(ColumnID);
+            datas.add(ColumnPrenom);
+            datas.add(ColumnNom);
+            datas.add(ColumnNote);
+
+            List<String> names = new ArrayList<String>();
+            names.add("ID");
+            names.add("Prenom");
+            names.add("Nom");
+            names.add("Note");
+
+            DataFrame df = new DataFrame(name, datas);
+
+        });
     }
+    
+    @Test
+    public void testnamesizePlusPetitVide() {
+        
+        assertDoesNotThrow(() -> {
+            Types typeInt = Types.INT;
+            Column<Integer> ColumnID = new Column<Integer>(typeInt);
+
+            Types typeStringNom = Types.STRING;
+            Column<String> ColumnNom = new Column<String>(typeStringNom);
+
+            Types typeStringPrenom = Types.STRING;
+            Column<String> ColumnPrenom = new Column<String>(typeStringPrenom);
+
+            Types typeFloat = Types.FLOAT;
+            Column<Float> ColumnNote = new Column<Float>(typeFloat);
+
+            for (int i = 0; i < 4; i++) {
+                ColumnID.add(i + 1);
+            }
+
+            ColumnPrenom.add("Alban");
+            ColumnPrenom.add("Gaetan");
+            ColumnPrenom.add("Fabien");
+            ColumnPrenom.add("Raphael");
+
+            ColumnNom.add("Faurie");
+            ColumnNom.add("Combe");
+            ColumnNom.add("Vial");
+            ColumnNom.add("Balssa");
+
+            ColumnNote.add((float) 45.2);
+            ColumnNote.add((float) 48.0);
+            ColumnNote.add((float) 0.1);
+            ColumnNote.add((float) 1257854.987457);
+
+            datas.add(ColumnID);
+            datas.add(ColumnPrenom);
+            datas.add(ColumnNom);
+
+            List<String> names = new ArrayList<String>();
+            names.add("ID");
+            names.add("Prenom");
+            names.add("Nom");
+            names.add("Note");
+
+            DataFrame df = new DataFrame(name, datas);
+        });
+    }
+
+    
+    @Test
+    public void testfichier3types() {
+        assertDoesNotThrow(() -> {
+            DataFrame df = new DataFrame("./src/test/resources/test3typesDiff.csv");
+        });
+    }
+
+    @Test
+    public void testfichier2Types() {
+
+        assertDoesNotThrow(() -> {
+            DataFrame df = new DataFrame("./src/test/resources/test2String1Int.csv");
+        });
+
+    }
+    
+    @Test
+    public void testfichierVide() {
+
+        assertDoesNotThrow(() -> {
+            DataFrame df = new DataFrame("./src/test/resources/testVide.csv");
+        });
+
+    }
+    
+    @Test
+    public void testfichierNullValue() {
+
+        assertDoesNotThrow(() -> {
+            DataFrame df = new DataFrame("./src/test/resources/testnullvaleur.csv");
+        });
+
+    }
+
+
+    @Test
+    public void whenExceptionThrown_thenAssertionSucceeds() {
+        Exception exception = assertThrows(TypeMismatchException.class, () -> {
+            DataFrame df = new DataFrame("./src/test/resources/testWrongType.csv");
+        });
+
+    }
+    
+
+    
+
 }
